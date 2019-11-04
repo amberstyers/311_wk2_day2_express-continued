3dc1d1ae9b6137db1c9085b7cbe16f709aabe5bc
amberstyers <amberstyers@gmail.com>
2019-11-04 02:18:30 -0600

    saving for now

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const app = express();
 
 const port = process.env.PORT || 4001;
 
+app.get("/products", (request, response) => {
+
+})
+app.post("/products", (request, response) => {
+
+})
+app.put("/products", (request, response) => {
+
+})
+app.delete("/products", (request, response) => {
+  
+})
+
 app.listen(port, () => {
  console.log(`Web server is listening on port ${port}!`);
 });
