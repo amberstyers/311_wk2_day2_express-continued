d7805f1c0c2cdd6f189b0964edddf0cd8196386b
amberstyers <amberstyers@gmail.com>
2019-11-04 03:12:31 -0600

    crashing

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const productRoutes = require("./routes/products");
+const commentRoutes = require("./routes/comments");
+const vehicleRoutes = require("./routes/vehicle");
+const contactRoutes = require("./routes/contacts");
 
 const port = process.env.PORT || 4001;
+
+// app.use(express.static('public'))
+
+
+// routes we use
+app.use(productRoutes);
+app.use(commentRoutes);
+app.use(vehicleRoutes);
+app.use(contactRoutes);
 
 app.get("/products", (request, response) => {
 
@@ -14,7 +27,7 @@ app.put("/products", (request, response) => {
 
 })
 app.delete("/products", (request, response) => {
-  
+
 })
 
 app.listen(port, () => {
